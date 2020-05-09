83fdb6c4af5ab4d209ea0e6df9bdcabc42427c69
John Medlock <42301475+johnMedlockDev@users.noreply.github.com>
2020-05-09 10:13:50 -0500

    it uploads a file correctly

--- src/main/java/com/github/johnmedlockdev/project1john/Application.java
+++ src/main/java/com/github/johnmedlockdev/project1john/Application.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Project1JohnApplication {
-
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Project1JohnApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
-
 }
