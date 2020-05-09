88b57d893f85d09b66c1789bb1a9a39e46a632b6
John Medlock <42301475+johnMedlockDev@users.noreply.github.com>
2020-05-09 12:15:30 -0500

    found out how to loop over a directory of files... might need to transform the list

--- src/main/java/com/github/johnmedlockdev/project1john/Application.java
+++ src/main/java/com/github/johnmedlockdev/project1john/Application.java
@@ -3,9 +3,20 @@ package com.github.johnmedlockdev.project1john;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(Application.class, args);
+
+
     }
 }
--- src/main/java/com/github/johnmedlockdev/project1john/controllers/FileUploadController.java
+++ src/main/java/com/github/johnmedlockdev/project1john/controllers/FileUploadController.java
@@ -1,39 +1,56 @@
 package com.github.johnmedlockdev.project1john.controllers;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
+import com.sun.xml.internal.bind.v2.TODO;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @RestController
-public class FileUploadController
-{
-	@RequestMapping(value = "/upload",
-			method = RequestMethod.POST,
-			consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+public class FileUploadController {
+    @RequestMapping(value = "/upload",
+            method = RequestMethod.POST,
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 
-	public RedirectView fileUpload(@RequestParam("file") MultipartFile file) throws IOException
-	{
-		File convertFile = new File("C:\\Storage\\" + file.getOriginalFilename());
+    public RedirectView fileUpload(@RequestParam("file") MultipartFile file) throws IOException {
+        File convertFile = new File("C:\\Storage\\" + file.getOriginalFilename());
 
-		try (FileOutputStream fout = new FileOutputStream(convertFile))
-		{
-			fout.write(file.getBytes());
-		}
-		catch (Exception exe)
-		{
-			exe.printStackTrace();
-		}
-		return new RedirectView("uploadForm");
-	}
+        try (FileOutputStream fout = new FileOutputStream(convertFile)) {
+            fout.write(file.getBytes());
+        } catch (Exception exe) {
+            exe.printStackTrace();
+        }
+        return new RedirectView("uploadForm"); // should go to d3 graph
+    }
+
+    @GetMapping("/files")
+    public List<String> displayFiles() throws IOException {
+        Path path = Paths.get("C:\\Storage\\");
+
+        Stream<Path> subPaths = Files.walk(path, 1);
+		List<String> subPathList = subPaths.filter(Files::isRegularFile)
+				.map(Objects::toString).collect(Collectors.toList());
+
+
+		//		System.out.println(subPathList);
+//            subPaths.filter(Files::isRegularFile).forEach(System.out::println);
+
+        return subPathList;
+    }
 
 }
+
 // // TODO: 5/9/2020 add a buffer to multipart
 // // TODO: 5/9/2020 route user based off success or failure of upload.
+//// TODO: 5/9/2020 add proper logging
