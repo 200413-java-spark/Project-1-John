be59d25b5de86ba5bd0780ae50eacf4cc73bc948
John Medlock <42301475+johnMedlockDev@users.noreply.github.com>
2020-05-13 18:21:30 -0500

    now the form and chart look better

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,4 +1,4 @@
-const mVar = 50;
+const mVar = 45;
 const margin = {top: mVar, right: mVar, bottom: mVar, left: mVar};
 const width = window.innerWidth - margin.left - margin.right - mVar / 2;
 const height = window.innerHeight - margin.top - margin.bottom - mVar / 2;
@@ -8,7 +8,7 @@ const svg = d3.select("#visualization")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")").attr("class", "graph-svg-component");
 
 const x = d3.scaleBand().range([0, width]);
 
@@ -23,11 +23,28 @@ d3.json('http://localhost:8080/graphApi').then((data) => {
 
     x.domain(data.map((d) => {
         return d.price
-    }));
+    })).padding(0.1);
+
     y.domain([0, d3.max(data, (d) => {
         return d.count;
     })]);
 
+
+    svg.append('g')
+        .selectAll("dot")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", (d) => {
+            return x(d.price);
+        })
+        .attr("width", x.bandwidth())
+        .attr("cy", (d) => {
+            return y(d.count)
+        })
+        .attr("height", (d) => {
+            return height - y(d.count)
+        })
 
     svg.selectAll(".bar")
         .data(data)
@@ -42,8 +59,8 @@ d3.json('http://localhost:8080/graphApi').then((data) => {
         })
         .attr("height", (d) => {
             return height - y(d.count)
-        })
-
+        }).style("fill", "#006B38FF")
+        .style("stroke", "#101820FF")
 
     svg.append("g")
         .attr("transform", `translate(0, ${height})`)
