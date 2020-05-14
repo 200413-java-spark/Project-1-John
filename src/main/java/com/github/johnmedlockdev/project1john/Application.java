b78ddc9785821e149591557b5cd49df28f2af0f0
John Medlock <42301475+johnMedlockDev@users.noreply.github.com>
2020-05-13 21:37:39 -0500

    made todos for tomorrow

--- src/main/java/com/github/johnmedlockdev/project1john/Application.java
+++ src/main/java/com/github/johnmedlockdev/project1john/Application.java
@@ -16,3 +16,10 @@ public class Application {
 //// TODO: 5/9/2020 add proper logging
 
 // TODO: 5/13/2020  maybe thing about refactoring the model so you don't have to drop the table everytime you hit the index
+// TODO: 5/13/2020 make tests
+// TODO: 5/13/2020 do logging the right way
+// TODO: 5/13/2020 add navigation bar
+// TODO: 5/13/2020 fill out documentation 
+// TODO: 5/13/2020 test jar
+// TODO: 5/13/2020 script
+// TODO: 5/13/2020 powerpoint
